7c9034c163eb6b242b755964d3765a41a9107e7e
Greidy Peña <greidykp@gmail.com>
2023-04-01 20:06:43 -0500

    Fix detail character

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -22,22 +22,31 @@ export default function Detail() {
 
   return (
     <div className={style.containerDetail}>
-      <div className={style.infoCharacter}>
-        {character.name ? (
-          <>
-            <p>Name: {character.name}</p>
-            <p>Status: {character.status}</p>
-            <p>Gender: {character.gender}</p>
-            <p>Specie: {character.specie}</p>
-            <p>Origin: {character.origin?.name}</p>
-          </>
-        ) : (
-          <p>Loading....</p>
-        )}
-      </div>
-      <div className={style.imgCharacter}>
-        {character.image && <img src={character.image} />}
-      </div>
+      {character.name ? (
+        <>
+        <div className={style.name}>
+            <h1>{character.name}</h1>
+        </div>
+        <div className={style.containerInfo}>
+          <div className={style.infoCharacter}>
+            <h1 className={character.status === "Alive" ? style.statusAlive : style.statusDead}> {character.status}</h1>
+            <div>
+            <h3>{character.gender}</h3>
+            <h4 className={style.gender}>Gender</h4>
+            <h3>{character.species}</h3>
+            <h4 className={style.species}>Species</h4>
+            <h3>{character.origin?.name}</h3>
+            <h4 className={style.origin}>Origin</h4>
+            </div>
+          </div>
+          <div className={style.imgCharacter}>
+             {character.image && <img src={character.image} />} {/* TODO: mostrar imagen por defecto */}
+          </div>
+        </div> 
+        </>
+      ) : (
+        <p>Loading....</p>
+      )}
     </div>
   );
 }
