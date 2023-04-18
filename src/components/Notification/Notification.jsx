c768d388ff473080bf88f3602c19764579904c36
Greidy Peña <greidykp@gmail.com>
2023-04-17 20:11:51 -0500

    Add notifications

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import styles from "./notification.module.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { hideNotificacion } from '../../redux/actions/actions';
+
+const Notification = ({message}) => {
+
+    const { notification } = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      setTimeout(() => {
+        dispatch(hideNotificacion())
+      }, 3100);
+    }, [])
+    
+    return (
+    <div class={styles.notification + " " + styles[notification.type]}>
+        <p>{notification.message}</p>
+        <span className={styles.notificationProgress}></span>
+    </div>
+  )
+}
+
+export default Notification
